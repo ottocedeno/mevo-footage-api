298391eb139a4b5fe5f6c583ec1ed46ffe97bbab
ottocedeno <otto@creativespaceclub.com>
2020-11-13 16:35:21 -0500

    Added presence validations for the video text inputs

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,10 @@
 class Video < ApplicationRecord
   belongs_to :category
   belongs_to :camera
+  
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :user_email, presence: true
+  validates :youtube_url, presence: true
+
 end
