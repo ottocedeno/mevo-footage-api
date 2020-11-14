00e5ac547bee4b38c6709455c0a6d892fc81baab
ottocedeno <otto@creativespaceclub.com>
2020-11-13 20:53:40 -0500

    Validates if youtube link is a valid YT link

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,7 +5,9 @@ class Video < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :user_email, presence: true
+  validates :user_email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be a valid email." } 
   validates :youtube_url, presence: true
+  validates :youtube_url, format: {with: /(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+/, message: "Must be a valid YouTube link."}
 
   validates :authorized_to_share, inclusion: {in: [true], message: "Must authorize usage of this video."}
 end
