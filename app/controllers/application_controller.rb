249e07811cbf1a7561f3091e1d0fdcd934154de6
ottocedeno <otto@creativespaceclub.com>
2020-11-15 15:54:23 -0500

    Application index action returns JSON for camera and category name data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def index
+    categories = Category.all_category_names
+    cameras = Camera.all_camera_models
+    render json: {categories: categories, cameras: cameras}, status: :ok
+  end
 end
