0731cce5e080db6d8a5adc4e6aeae3defa8a97c1
ottocedeno <otto@creativespaceclub.com>
2020-11-15 13:34:08 -0500

    Video instance method: errors_to_strings, maps errors object to array of simple error strings

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,16 +4,30 @@ class Video < ApplicationRecord
 
   before_create :set_verified_to_false
   
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :user_email, presence: true
+  validates :title, presence: {message: "Title can't be blank"}
+  validates :description, presence: {message: "Description can't be blank"}
+  validates :user_email, presence: {message: "Email can't be blank"}
   validates :user_email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be a valid email." } 
-  validates :youtube_url, presence: true
+  validates :youtube_url, presence: {message: "Youtube URL can't be blank"}
   validates :youtube_url, format: {with: /(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+/, message: "Must be a valid YouTube link."}
 
   validates :authorized_to_share, inclusion: {in: [true], message: "Must authorize usage of this video."}
 
+  def errors_to_strings
+    error_strings = []
+    if !!self.errors
+      self.errors.messages.each_value do |msgArray|
+        msgArray.each {|msg| error_strings << msg} 
+      end
+    end
+
+    error_strings
+  end
+
+  private
   def set_verified_to_false
     self.verified = false
   end
+
+  
 end
